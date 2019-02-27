ede3291589736b8ac147a607715157c3840e0c1d
Diptis Halder <diptis.halder@gmail.com>
2019-02-27 16:57:05 +0000

    :alembic: test: only clicking on Sign Up throws an error

--- spec/features/only_clicking_sign_up_throws_error_spec.rb
+++ spec/features/only_clicking_sign_up_throws_error_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature "Sign up", type: :feature do
   scenario "only clicking on Sign Up throws an error" do
     visit "/users/sign_up"
+    # dummy comment
     fill_in "user[email]", with: ""
     fill_in "user[password]", with: ""
     fill_in "user[password_confirmation]", with: ""
