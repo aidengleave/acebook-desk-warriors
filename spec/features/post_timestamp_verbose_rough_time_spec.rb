3a97d5e91ba0e93e8da069455d5043595a5240a9
Diptis Halder <diptis.halder@gmail.com>
2019-03-07 15:32:24 +0000

    :white_check_mark: feat: Done verbose timestamp on posts + rubocop fixes

--- spec/features/post_timestamp_verbose_rough_time_spec.rb
+++ spec/features/post_timestamp_verbose_rough_time_spec.rb
@@ -10,15 +10,15 @@ RSpec.feature "Post timestamp", type: :feature do
     expect(page).to have_content("Add a new post")
   end
 
-  scenario "test that post shows '1 minute ago' if current time is 1 minute ahead of post time" do
+  scenario "test that post shows 'Just now' if 5 secs has elapsed since post created" do
     new_sign_up("easyname", "easyemail@easy.com", "abc123")
     expect(page.status_code).to eql 200
     expect(page).to have_content("Add a new post")
     fill_in "post[message]", :with => 'lots of shizzle me nizzle test message'
     click_button "Add post"
     expect(page).to have_content("shizzle me nizzle")
-    sleep (5)
-    # expect(page).to have_content("1 minute ago")
+    sleep 5
+    expect(page).to have_content("Just now")
   end
 
 end
