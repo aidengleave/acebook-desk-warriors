cf82a0059ef25ba4adb27ecc83c9b4682fbe2985
Sherif Shendidy <sherif@shendidy.com>
2019-03-08 12:41:27 +0000

    :sparkle: feat: can chat with other signed in users but require page reloading

--- app/controllers/messges_controller.rb
+++ app/controllers/messges_controller.rb
@@ -1,7 +1,20 @@
 class MessgesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @messges = Messge.all
+    @messge = Messge.new
   end
-  
+
   def create
+    @messge = Messge.new(messge_params)
+    @messge.user = current_user
+    @messge.save
+  end
+
+  private
+
+  def messge_params
+    params.require(:messge).permit(:body)
   end
 end
